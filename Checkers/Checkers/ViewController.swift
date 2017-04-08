2dfe2da45d0ecbb7e966d560c0406870346629e1
pittNearsoft <projas@nearsoft.com>
2017-04-07 19:03:30 -0700

    First commit

---
+++ Checkers/Checkers/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Checkers
+//
+//  Created by projas on 4/7/17.
+//  Copyright © 2017 nearsoft. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
